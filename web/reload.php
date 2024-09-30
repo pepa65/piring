5a50ca147fe06dedc1c8a21c6e43091614f49c09
gitlab.com/pepa65 <pepa65@passchier.net>
2024-09-30 07:58:55 +0700

    pre on restart

--- web/reload.php
+++ web/reload.php
@@ -4,6 +4,6 @@ if($_GET['key']=='guyftykfumgfkdrgnkvgfnbtuibgtivgithggcrtgtmighicvg'){
 	echo(system('killall -q -w -9 python2.7'));
 	chdir($_SERVER['DOCUMENT_ROOT'].'/..'); // Location of files
 	echo(system('~/git/piring/getcsv -d'));
-	exec('~/git/piring/ring >~/git/piring/web/log &');
+	exec('echo "<pre>" >~/git/piring/web/log; ~/git/piring/ring >>~/git/piring/web/log &');
 }
 ?>
